28035f82f738bc4f28f5a9b0560a42db83c46609
saifulalamfahime <you@example.com>
2022-09-21 19:11:35 +0600

    full project

---
+++ src/data.js
@@ -0,0 +1,31 @@
+let shopItemData = [
+    {
+        id: "dsbvgjkhsd",
+        name: "gun789",
+        price: 4567,
+        desc: "lorem ipsum doler sit amet consectedtur adipisicing",
+        img: "gun-1.jpg"
+    },
+    {
+        id: "ydhfguvsd",
+        name: "akm452",
+        price: 3067,
+        desc: "lorem ipsum doler sit amet consectedtur adipisicing",
+        img: "gun-2.jpg"
+    },
+    {
+        id: "dsbvgjdfsd",
+        name: "mi7823",
+        price: 4007,
+        desc: "lorem ipsum doler sit amet consectedtur adipisicing",
+        img: "gun-3.jpg"
+    },
+    {
+        id: "dsbdsfghsd",
+        name: "mahinagun789",
+        price: 2597,
+        desc: "lorem ipsum doler sit amet consectedtur adipisicing",
+        img: "gun-4.jpg"
+    },
+    
+];
